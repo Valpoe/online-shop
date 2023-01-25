1f0d92501a4f8ce2c847b73b01d70edd68cd79d0
AndersTuomainen <tuomainen.anders@outlook.com>
2023-01-25 12:34:37 +0200

    testi

---
+++ testi.cs
@@ -0,0 +1,44 @@
+//create me calculator app
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Calculator");
+            Console.WriteLine("Enter the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the operation");
+            string operation = Console.ReadLine();
+            int result = 0;
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+            }
+            Console.WriteLine("Result is " + result);
+            Console.ReadLine();
+        }
+    }
+}
