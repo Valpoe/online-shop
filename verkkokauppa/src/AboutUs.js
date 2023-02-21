97d1afdb502aa52b0707ae037d96a74781aebeca
millaetirkkonen <122272294+millaetirkkonen@users.noreply.github.com>
2023-02-21 23:01:42 +0200

    About Us kuva ja kuville Images folder

--- verkkokauppa/src/AboutUs.js
+++ verkkokauppa/src/AboutUs.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import kouluTarvikkeet from "./Images/KouluTarvikkeetKuva.png";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function AboutUs (){
     return(
-        <div className="bg-danger p-3 bg-opacity-25">
-            <h4 class="font-weight-bold">About Us</h4>
-            <p class="text-left">We are School store, an online retailer specializing in providing high-quality school-related
+        <section className="bg-danger p-3 bg-opacity-25">
+        <div className="container">
+            <div className="row">
+            <div className="col-md-8">
+            <h4 className="fw-bold text-left">About Us</h4>
+            <p className="text-left fs-5">We are School store, an online retailer specializing in providing high-quality school-related
                 products to customers all over the world. Our mission is to bring joy and excitement to students
                 by offering a wide range of school items at affordable prices. Our team is comprised of experts
                 and enthusiasts who are dedicated to sourcing only the best school products from around the globe.
@@ -14,8 +21,13 @@ function AboutUs (){
                 we carefully curate our product offerings to ensure that we only carry the best of the best. Whether you're
                 a proud student or simply a fan of school, you can rest assured that when you shop with us, you're getting the very best.
             </p>
+            </div>
+            <div className="col-md-4 d-flex align-items-end">
+            <img width="400" height="400" src={kouluTarvikkeet} alt="koulutarvikkeet"></img>
+            </div>
+            </div>
         </div>
-
+        </section>
     );
 
 }
