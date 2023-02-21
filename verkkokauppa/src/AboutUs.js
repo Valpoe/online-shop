dd5112b468073caab3cce0e94499595d363c60e8
millaetirkkonen <122272294+millaetirkkonen@users.noreply.github.com>
2023-02-21 16:04:11 +0200

    About us teksit

--- verkkokauppa/src/AboutUs.js
+++ verkkokauppa/src/AboutUs.js
@@ -3,8 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function AboutUs (){
     return(
-        <div>
-
+        <div className="bg-danger p-3 bg-opacity-25">
+            <h4 class="font-weight-bold">About Us</h4>
+            <p class="text-left">We are School store, an online retailer specializing in providing high-quality school-related
+                products to customers all over the world. Our mission is to bring joy and excitement to students
+                by offering a wide range of school items at affordable prices. Our team is comprised of experts
+                and enthusiasts who are dedicated to sourcing only the best school products from around the globe.
+                From toys and apparel to accessories and collectibles, we have something for everyone who school supplys.
+                We understand that our customers are looking for unique and high-quality weasel items, which is why
+                we carefully curate our product offerings to ensure that we only carry the best of the best. Whether you're
+                a proud student or simply a fan of school, you can rest assured that when you shop with us, you're getting the very best.
+            </p>
         </div>
 
     );
