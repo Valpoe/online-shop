8e38f57289786652080d2df2364d349598943878
millaetirkkonen <122272294+millaetirkkonen@users.noreply.github.com>
2023-02-22 00:20:32 +0200

    AboutUs sivua lisää

--- verkkokauppa/src/AboutUs.js
+++ verkkokauppa/src/AboutUs.js
@@ -1,33 +1,48 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import kouluTarvikkeet from "./Images/KouluTarvikkeetKuva.png";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function AboutUs (){
+
+    
+
     return(
-        <section className="bg-danger p-3 bg-opacity-25">
-        <div className="container">
-            <div className="row">
-            <div className="col-md-8">
-            <h4 className="fw-bold text-left">About Us</h4>
-            <p className="text-left fs-5">We are School store, an online retailer specializing in providing high-quality school-related
-                products to customers all over the world. Our mission is to bring joy and excitement to students
-                by offering a wide range of school items at affordable prices. Our team is comprised of experts
-                and enthusiasts who are dedicated to sourcing only the best school products from around the globe.
-                From toys and apparel to accessories and collectibles, we have something for everyone who school supplys.
-                We understand that our customers are looking for unique and high-quality weasel items, which is why
-                we carefully curate our product offerings to ensure that we only carry the best of the best. Whether you're
-                a proud student or simply a fan of school, you can rest assured that when you shop with us, you're getting the very best.
-            </p>
-            </div>
-            <div className="col-md-4 d-flex align-items-end">
-            <img width="400" height="400" src={kouluTarvikkeet} alt="koulutarvikkeet"></img>
-            </div>
-            </div>
+        <div>
+            <section>
+                <nav className="navbar navbar-light bg-warning p-3 bg-opacity-25">
+                    <h2>Navbar</h2>        
+                </nav>
+            </section>
+
+            <section className="bg-warning p-3 bg-opacity-25">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8">
+                            <h4 className="fw-bold text-left">About Us</h4>
+                            <p className="text-left fs-5">We are School store, an online retailer specializing in providing high-quality school-related
+                            products to customers all over the world. Our mission is to bring joy and excitement to students
+                            by offering a wide range of school items at affordable prices. Our team is comprised of experts
+                            and enthusiasts who are dedicated to sourcing only the best school products from around the globe.
+                            From toys and apparel to accessories and collectibles, we have something for everyone who school supplys.
+                            </p>
+                            <p className="text-left fs-5">
+                            We understand that our customers are looking for unique and high-quality weasel items, which is why
+                            we carefully curate our product offerings to ensure that we only carry the best of the best. Whether you're
+                            a proud student or simply a fan of school, you can rest assured that when you shop with us, you're getting the very best.
+                            Thank you for choosing our school supply store. We look forward to serving you and helping you achieve your academic goals.
+                            </p>
+                            <p className="text-left fs-5">
+                            In addition to providing high-quality products and exceptional service, we also support the local community through various
+                            charitable initiatives. We believe that education is the key to success, and we are committed to making it accessible to everyone, regardless of their background or financial circumstances.
+                            </p>
+                        </div>
+                        <div className="col-md-4 d-flex align-items-center">
+                            <img width="450" height="450" src={kouluTarvikkeet} alt="koulutarvikkeet"></img>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
-        </section>
     );
 
 }
