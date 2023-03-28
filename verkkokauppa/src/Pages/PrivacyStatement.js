1b746a8f6a1091574dc358565c27f598d5101cb0
millaetirkkonen <122272294+millaetirkkonen@users.noreply.github.com>
2023-03-28 23:49:40 +0300

    PrivacyStatement MDB

--- verkkokauppa/src/Pages/PrivacyStatement.js
+++ verkkokauppa/src/Pages/PrivacyStatement.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import "../css/PrivacyStatement.css";
+import {
+    MDBTypography,
+    MDBIcon
+  } from 'mdb-react-ui-kit';
 
 const PrivacyStatement = () => {
 
 return(
 
-<div>
-    
-    <h1 className="PSheader">REKISTERI- & TIETOSUOJASELOSTE</h1>
-    <p className="Paivitys">Päivitetty 21.3.2023</p>
-    <div className="text">
+<MDBTypography tag="div" className="p-5">
+    <MDBTypography>
+        <MDBTypography tag='strong' variant='h1' className='t-5 PSheader'>REKISTERI- & TIETOSUOJASELOSTE</MDBTypography>
+    </MDBTypography>
+    <MDBTypography tag="em" className="ps-4 text">Päivitetty 21.3.2023</MDBTypography>
+    <MDBTypography tag="div" className="lh-lg ps-4 pt-2 text">
     Tämän sivun tarkoituksena on auttaa sinua ymmärtämään millaisia tietoja keräämme kun selaat sivustojamme ja mikä on tietojenkeruun tarkoitus.
 
     Tämä on YRITYS EU:n yleisten tietosuoja-asetusten (GDPR) mukainen rekisteri- ja tietosuojaseloste.
@@ -19,20 +24,16 @@ return(
     Sivustolla kerättyjä selaajatietoja käytetään sivuston palvelujen parantamiseen. Esimerkkinä mainittakoon kohdennettu mainonta. Tietojesi avulla voimme näyttää sinulle ajankohtaisempaa mainontaa ja hakutuloksia. Tietojesi avulla voimme myös analysoida sivuston liikennettä, jonka avulla voimme kehittää sivuston sisältöä ja toimivuutta paremmaksi.
 
     Selatessasi sivujamme haluamme sinun ymmärtävän, kuinka käytämme tietoa ja tietävän, että meille sinun tietojesi turvallisuus on tärkeä asia!
-    </div>
-    <h5 className="PSheader">1. Rekisterinpitäjä</h5>
+    </MDBTypography>
+    <MDBTypography variant="h5" className="PSheader pb-4">1. Rekisterinpitäjä</MDBTypography>
+    <MDBTypography variant="h5" className="PSheader pb-4">2. Rekisteristä vastaava yhteyshenkilö</MDBTypography>
+    <MDBTypography variant="h5" className="PSheader">3. Rekisterin nimi</MDBTypography>
+    
+    <MDBTypography tag="div" className="lh-lg ps-4 pt-2 text">YRITYS Oy:n markkinointi- ja analytiikkarekisteri.</MDBTypography>
 
-
-    <h5 className="PSheader">2. Rekisteristä vastaava yhteyshenkilö</h5>
-
-
-    <h5 className="PSheader">3. Rekisterin nimi</h5>
-    <div className="text">
-    YRITYS Oy:n markkinointi- ja analytiikkarekisteri.
-    </div>
-
-    <h5 className="PSheader">4. Oikeusperuste ja henkilötietojen käsittelyn tarkoitus</h5>
-    <div className="text">
+    <MDBTypography variant="h5" className="PSheader">4. Oikeusperuste ja henkilötietojen käsittelyn tarkoitus</MDBTypography>
+    
+    <MDBTypography tag="div" className="lh-lg ps-4 pt-2 text">
     Tietojenkäsittelylle on oikeusperuste EU:n yleisen tietosuoja-asetuksen mukaisesti.
 
     Rekisterin tarkoituksena on rekisterinpitäjän ja asiakkaan välisen asiakassuhteen hoitaminen, sekä markkinointi ja viestintä. Rekisterin hallinta ja tietojen käsittely voidaan ulkoistaa rekisterinpitäjän yhteistyökumppanille, joka on sitoutunut noudattamaan tätä rekisteriselostetta.
@@ -40,57 +41,81 @@ return(
     Vierailija antaa suostumuksensa henkilötietojensa käsittelyyn tässä tietosuojaselosteessa mainittuihin tarkoituksiin lähettämällä tiedot yhteydenottokaavakkeen kautta.
 
     Tietoja ei käytetä automatisoituun päätöksentekoon tai profilointiin.
-    </div>
+    </MDBTypography>
 
-    <h5 className="PSheader">5. Rekisterin tietosisältö</h5>
-    <div className="text">
-    <div>Tämän sivuston artikkelit voivat sisältää upotettua sisältöä (esim. videoita, kuvia, artikkeleita jne.). Muiden verkkosivustojen upotettu sisältö käyttäytyy täsmälleen samalla tavalla kuin jos vierailija olisi vieraillut toisella verkkosivustolla.</div>
-    <br></br>
-    <div>Rekisteri saattaa sisältää seuraavia tietoja asiakkaasta:</div>
-    <ul>
-        <li>Nimi</li>
-        <li>Puhelinnumero</li>
-        <li>Sähköposti</li>
-        <li>IP-osoite</li>
-        <li>Mahdolliset annetut lisätiedot</li>
-    </ul>
+    <MDBTypography variant="h5" className="PSheader">5. Rekisterin tietosisältö</MDBTypography>
 
+    <MDBTypography tag="div" className="lh-lg ps-4 pt-2 text">
+    <MDBTypography tag="div" className="pb-3">Tämän sivuston artikkelit voivat sisältää upotettua sisältöä (esim. videoita, kuvia, artikkeleita jne.).
+        Muiden verkkosivustojen upotettu sisältö käyttäytyy täsmälleen samalla tavalla kuin jos vierailija olisi vieraillut
+        toisella verkkosivustolla.
+    </MDBTypography>
+   
+   <MDBTypography listUnStyled className="mb-0 pb-3">
+        Rekisteri saattaa sisältää seuraavia tietoja asiakkaasta:
+    
+    <li className="mb-1">
+        <MDBIcon icon="arrow-right" className="me-2 text-warning"/>Nimi
+    </li>
+    <li className="mb-1">
+        <MDBIcon icon="arrow-right" className="me-2 text-warning"/>Puhelinnumero
+    </li>
+    <li className="mb-1">
+        <MDBIcon icon="arrow-right" className="me-2 text-warning"/>Sähköposti
+    </li>
+    <li className="mb-1">
+        <MDBIcon icon="arrow-right" className="me-2 text-warning"/>IP-osoite
+    </li>
+    <li className="mb-1">
+        <MDBIcon icon="arrow-right" className="me-2 text-warning"/>Mahdolliset annetut lisätiedot
+    </li> 
+    </MDBTypography>
     Verkkosivuston vierailijoiden IP-osoitteita ja palvelun toiminnoille välttämättömiä evästeitä käsitellään oikeutetun edun perusteella mm. tietoturvasta huolehtimiseksi ja sivuston vierailijoiden tilastotietojen keruuta varten niissä tapauksissa, kun niiden voidaan katsoa olevan henkilötietoja. Kolmansien osapuolten evästeille pyydetään tarvittaessa suostumus erikseen.
-    </div>
-    <h5 className="PSheader">6. Säännönmukaiset tietolähteet</h5>
-    <div className="text">
+    </MDBTypography>
+
+    <MDBTypography variant="h5" className="PSheader">6. Säännönmukaiset tietolähteet</MDBTypography>
+
+    <MDBTypography tag="div" className="lh-lg ps-4 pt-2 text">
     Rekisteri koostetaan rekisterinpitäjän asiakastietojärjestelmästä sekä sivuston kävijäseurantatyökalusta, yleisesti saatavilla olevista internet-lähteistä ja muista julkisista sekä maksullisista, että maksuttomista lähteistä.
 
     Rekisteriin tallennettavat tiedot saadaan asiakkaalta mm. www-lomakkeilla lähetetyistä viesteistä, sähköpostitse, puhelimitse, sosiaalisen median palvelujen kautta, sopimuksista, asiakastapaamisista sekä sivuston kävijäseurantatyökalusta, yleisesti saatavilla olevista internet-lähteistä ja muista julkisista sekä maksullisista, että maksuttomista lähteistä ja muista tilanteista, joissa asiakas luovuttaa tietojaan.
 
     Yritysten ja muiden organisaatioiden yhteyshenkilöiden tietoja voidaan kerätä myös julkisista lähteistä kuten verkkosivuilta, hakemistopalveluista ja muilta yrityksiltä.
-    </div>
-    <h5 className="PSheader">7. Tietojen säännönmukaiset luovutukset ja tietojen siirto EU:n tai ETA:n ulkopuolelle</h5>
-    <div className="text">
+    </MDBTypography>
+
+    <MDBTypography variant="h5" className="PSheader">7. Tietojen säännönmukaiset luovutukset ja tietojen siirto EU:n tai ETA:n ulkopuolelle</MDBTypography>
+    
+    <MDBTypography tag="div" className="lh-lg ps-4 pt-2 text">
     Rekisterinpitäjä ei luovuta tietoja ulkopuolisille lukuun ottamatta kohdassa 4 mainittua rekisterinpitäjän yhteistyökumppania, paitsi Suomen viranomaistoimien niin edellyttäessä.
 
     Tietoja ei luovuteta säännönmukaisesti muille tahoille. Tietoja voidaan julkaista siltä osin kuin niin on sovittu asiakkaan kanssa.
 
     Tietoja voidaan siirtää rekisterinpitäjän toimesta myös EU:n tai ETA:n ulkopuolelle. Tietoja ei siirretä Yhdysvaltoihin ilman rekisteröityjen nimenomaista suostumusta.
-    </div>
-    <h5 className="PSheader">8. Rekisterin suojauksen periaatteet</h5>
-    <div className="text">
+    </MDBTypography>
+
+    <MDBTypography variant="h5" className="PSheader">8. Rekisterin suojauksen periaatteet</MDBTypography>
+   
+    <MDBTypography tag="div" className="lh-lg ps-4 pt-2 text">
     Rekisterin käsittelyssä noudatetaan huolellisuutta ja tietojärjestelmien avulla käsiteltävät tiedot suojataan asianmukaisesti. Kun rekisteritietoja säilytetään Internet-palvelimilla, niiden laitteiston fyysisestä ja digitaalisesta tietoturvasta huolehditaan asiaankuuluvasti. Rekisterinpitäjä huolehtii siitä, että tallennettuja tietoja sekä palvelimien käyttöoikeuksia ja muita henkilötietojen turvallisuuden kannalta kriittisiä tietoja käsitellään luottamuksellisesti ja vain niiden työntekijöiden toimesta, joiden työnkuvaan se kuuluu.
-    </div>
-    <h5 className="PSheader">9. Tarkastusoikeus ja oikeus vaatia tiedon korjaamista</h5>
-    <div className="text">
+    </MDBTypography>
+
+    <MDBTypography variant="h5" className="PSheader">9. Tarkastusoikeus ja oikeus vaatia tiedon korjaamista</MDBTypography>
+    
+    <MDBTypography tag="div" className="lh-lg ps-4 pt-2 text">
     Jokaisella rekisterissä olevalla henkilöllä on oikeus tarkistaa rekisteriin tallennetut tietonsa ja vaatia mahdollisen virheellisen tiedon korjaamista tai puutteellisen tiedon täydentämistä. Mikäli henkilö haluaa tarkistaa hänestä tallennetut tiedot tai vaatia niihin oikaisua, pyyntö tulee lähettää kirjallisesti rekisterinpitäjälle. Rekisterinpitäjä voi pyytää tarvittaessa pyynnön esittäjää todistamaan henkilöllisyytensä. Rekisterinpitäjä vastaa asiakkaalle EU:n tietosuoja-asetuksessa säädetyssä ajassa (pääsääntöisesti kuukauden kuluessa).
 
     Voit myös pyytää, että poistamme kaikki hallussamme olevat henkilötiedot. Tämä ei sisällä tietoja, joita meidän on säilytettävä hallinnollisia, oikeudellisia tai turvallisuustarkoituksia varten.
-    </div>
-    <h5 className="PSheader">10. Evästeet</h5>
-    <div className="text">
+    </MDBTypography>
+
+    <MDBTypography variant="h5" className="PSheader">10. Evästeet</MDBTypography>
+
+    <MDBTypography tag="div" className="lh-lg ps-4 pt-2 text">
     Rekisterinpitäjän sivuilla käytetään evästeitä käyttökokemuksen edistämiseksi ja sivuston kävijämäärien seuraamiseksi. 
 
     Eväste on tekstitiedosto, joka verkkosivuilla käydessä tallentuu käyttäjän tietokoneelle. Se sisältää tietoja ja sitä käytetään mm. markkinointiin ja sivuston kehittämiseen. Evästeiden avulla kerätään tietoa sivuston käytöstä. Käyttäjän henkilöllisyys ei käy ilmi evästeiden avulla. Käyttäjä voi poistaa evästeet käytöstä selaimen asetuksista. Rekisterinpitäjä ei takaa, että sivusto toimii oikein evästeiden poistamisen jälkeen.
-    </div>
+    </MDBTypography>
 
-</div>
+</MDBTypography>
 
 );
 
