ee7c4294deed123087f712d93654e639c92fc442
Mikko Rautava <mikko.rautava@edu.savonia.fi>
2023-03-30 23:50:13 +0300

    etusivu konteksti

--- verkkokauppa/src/Pages/Etusivu.js
+++ verkkokauppa/src/Pages/Etusivu.js
@@ -6,7 +6,15 @@ return(
 
 <div>
     <nav className="navbar navbar-light bg-light">
-        Etusivu       
+        <h1>NAPSUKAUPPA</h1>
+        <p>
+            Koulutarvikkeet ovat olennainen osa koulunkäyntiä ja niitä tarvitaan monissa eri tilanteissa. Kynät, kumi, viivotin, ja paperit ovat esimerkkejä perus koulutarvikkeista, jotka oppilaiden on yleensä tuotava mukanaan kouluun päivittäin. Nämä tarvikkeet ovat välttämättömiä oppilaille, jotta he voivat kirjoittaa muistiinpanoja ja suorittaa tehtäviä.
+            Lisäksi, joillakin oppiaineilla on erityisiä vaatimuksia tarvikkeiden suhteen. Esimerkiksi matematiikassa tarvitaan usein kulmaviivoitinta, pyöristäjää ja harppia, kun taas kuvataidekurssilla tarvitaan erilaisia värikyniä, pensseleitä ja maaleja.
+
+            Me tarjoamme koulutarvikkeita siksi, että haluamme auttaa oppilaita menestymään koulussa ja tarjota heille laadukkaita tarvikkeita kohtuulliseen hintaan. Haluamme varmistaa, että kaikilla oppilailla on käytettävissään tarvittavat työkalut ja että heillä on pääsy kaikenlaisiin tarvikkeisiin, joita he saattavat tarvita koulussa.
+            Meidän tarjoamamme koulutarvikkeet ovat laadukkaita ja kestäviä, joten oppilaat voivat käyttää niitä pitkään. Lisäksi meillä on laaja valikoima erilaisia tarvikkeita, joten oppilaat voivat löytää kaiken, mitä he tarvitsevat yhdeltä paikalta. Tarjoamme myös hyvät hinnat, jotta kaikilla oppilailla on varaa hankkia tarvitsemansa tarvikkeet.
+            Kokonaisuudessaan koulutarvikkeet ovat tärkeitä koulunkäynnin kannalta ja me tarjoamme niitä, jotta oppilailla on kaikki tarvittavat työkalut menestyäkseen koulussa.                
+        </p>
     </nav>
 </div>
 
