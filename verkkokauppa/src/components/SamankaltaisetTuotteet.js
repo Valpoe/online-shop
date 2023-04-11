e727569b722a0e6a8775f8755f70b7ad61296524
Mikko Rautava <mikko.rautava@edu.savonia.fi>
2023-04-11 16:12:47 +0300

    samankaltaiset tuotteet - ei näytä päätuotetta toiminnallisuus

--- verkkokauppa/src/components/SamankaltaisetTuotteet.js
+++ verkkokauppa/src/components/SamankaltaisetTuotteet.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { MDBRow, MDBCol, MDBCard, MDBCardImage, MDBCardBody, MDBBtn, MDBInput, MDBCardHeader, MDBCardTitle, MDBCardText, MDBCardFooter, MDBContainer, MDBSpinner, MDBIcon } from 'mdb-react-ui-kit';
+import { MDBCol, MDBCard, MDBCardImage, MDBCardBody, MDBCardHeader, MDBCardTitle, MDBCardText, MDBCardFooter, MDBIcon } from 'mdb-react-ui-kit';
 import { getKategoriaTuotteet } from "./Server/TuoteAPI";
 
 function SamankaltaisetTuotteet(props) {
@@ -11,52 +11,77 @@ function SamankaltaisetTuotteet(props) {
       }
 
       const [tuotekategoria, setTuotekategoria] = useState([props.tuotekategoria]);
+      const [valittuTuote, setvalittuTuote] = useState([props.aktiivinenTuote]);
+      const [filteredTuote, setFilteredTuote] = useState([props.tuotekategoria]);
 
       useEffect(() => {
         async function TuoteKategoriaHaku() {
           if (props.tuotekategoria && props.tuotekategoria) {
-            console.log(props.tuote.tuotekategoria + "  kategoria id haettu!!!");
-            setTuotekategoria(await getKategoriaTuotteet(props.tuotekategoria));
+              setTuotekategoria(await getKategoriaTuotteet(props.tuotekategoria));
+              console.log(tuotekategoria + "  kategoria id haettu!!!");
+              setvalittuTuote(props.tuote[0].tuoteID);
           }
         }
         TuoteKategoriaHaku();
-      
+
       }, [props.tuotekategoria]);
-      
 
+      useEffect(() => {
+        console.log("tuotekategoria: " + tuotekategoria + " aktiivinenTuote: " + props.tuote[0].tuoteID)
+        //remove valittutuote from tuotekategoria
 
-return (
-    <>
-        {tuotekategoria.map((tuotteet) => (
-                    <MDBCol key={tuotteet.tuoteID}>
-                    <MDBCard className="h-100">
-                        <MDBCardImage
-                        src={tuotteet.kuva}
-                        position="top"
-                        alt="..."
-                        />
-                        <MDBCardBody>
-                        <MDBCardHeader>
-                                <MDBCardTitle>{tuotteet.tuotenimi}</MDBCardTitle>
-                                </MDBCardHeader>
-                            <MDBCardBody>
-                                <MDBCardText>
-                                Saldo: {tuotteet.varastosaldo}
-                                </MDBCardText>
-                                <MDBCardText>
-                                <NavLink to={`/tuotteet/${tuotteet.tuoteID}`} onClick={() => props.setAktiivinenTuote(tuotteet.tuoteID)}>
-                                    Lisätietoja
-                                </NavLink>
-                                </MDBCardText>
-                                <MDBCardText><button className='btn btn-success' onClick={() => HandleAddToCart(tuotteet)}>Lisää ostoskoriin</button></MDBCardText>
-                            </MDBCardBody>
-                            <MDBCardFooter className="fw-bold">Hinta: {tuotteet.hinta} <MDBIcon fas icon="euro-sign" /></MDBCardFooter>
-                        </MDBCardBody>
-                    </MDBCard>
-                    </MDBCol>
-                        ))}
-    </>
- )
+    }, [valittuTuote]);
+
+      useEffect(() => {
+        console.log("filteredtuote: " + props.aktiivinenTuote);
+        refreshPage();
+    }, [props.aktiivinenTuote]);
+
+      const refreshPage = () => {
+        //move to top of page
+        window.scrollTo(0, 0);
+      }
+
+      return (
+        <>
+          {tuotekategoria
+            .filter((tuote) => tuote.tuoteID !== props.aktiivinenTuote)
+            .map((tuotteet, index) => (
+              <MDBCol key={index}>
+                <MDBCard className="h-100">
+                  <MDBCardImage src={tuotteet.kuva} position="top" alt="..." />
+                  <MDBCardBody>
+                    <MDBCardHeader>
+                      <MDBCardTitle>{tuotteet.tuotenimi}</MDBCardTitle>
+                    </MDBCardHeader>
+                    <MDBCardBody>
+                      <MDBCardText>Saldo: {tuotteet.varastosaldo}</MDBCardText>
+                      <MDBCardText>
+                        <NavLink
+                          to={`/tuotteet/${tuotteet.tuoteID}`}
+                          onClick={() => props.setAktiivinenTuote(tuotteet.tuoteID)}
+                        >
+                          Lisätietoja
+                        </NavLink>
+                      </MDBCardText>
+                      <MDBCardText>
+                        <button
+                          className="btn btn-success"
+                          onClick={() => HandleAddToCart(tuotteet)}
+                        >
+                          Lisää ostoskoriin
+                        </button>
+                      </MDBCardText>
+                    </MDBCardBody>
+                    <MDBCardFooter className="fw-bold">
+                      Hinta: {tuotteet.hinta} <MDBIcon fas icon="euro-sign" />
+                    </MDBCardFooter>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBCol>
+            ))}
+        </>
+      );
 }
 
 export default SamankaltaisetTuotteet;
