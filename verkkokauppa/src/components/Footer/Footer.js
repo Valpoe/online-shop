2f8a8a9842601068df16df8dc849d3945996ebfc
millaetirkkonen <122272294+millaetirkkonen@users.noreply.github.com>
2023-04-21 20:05:19 +0300

    footer suomennoksia

--- verkkokauppa/src/components/Footer/Footer.js
+++ verkkokauppa/src/components/Footer/Footer.js
@@ -63,25 +63,25 @@ return (
             </MDBCol>
 
             <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
+              <h6 className='text-uppercase fw-bold mb-4 text-dark'>Hyödylliset linkit</h6>
               <p>
                 <a href='#!' className='text-reset'>
-                  Pricing
+                  Hinnoittelu
                 </a>
               </p>
               <p>
                 <a href='#!' className='text-reset'>
-                  Settings
+                  Asetukset
                 </a>
               </p>
               <p>
                 <a href='#!' className='text-reset'>
-                  Orders
+                  Lue lisää
                 </a>
               </p>
               <p>
                 <a href='#!' className='text-reset'>
-                  Help
+                  Tuki
                 </a>
               </p>
             </MDBCol>
