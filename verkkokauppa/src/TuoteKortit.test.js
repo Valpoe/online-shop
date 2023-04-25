fa18ce119d80edad6113435cfd9622f720b35d38
Anders Tuomainen <anders.tuomainen@edu.savonia.fi>
2023-04-25 16:13:55 +0300

    asd

--- verkkokauppa/src/TuoteKortit.test.js
+++ verkkokauppa/src/TuoteKortit.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import TuoteKortit from './components/TuoteKortit';
 import { MemoryRouter } from 'react-router-dom';
 
