87e8e215cff662323ae14704859f73f2672c454e
Mikko Rautava <mikko.rautava@edu.savonia.fi>
2023-04-25 12:50:25 +0300

    edited asiakashallinta

--- verkkokauppa/src/Pages/AccountManagement.js
+++ verkkokauppa/src/Pages/AccountManagement.js
@@ -1,4 +1,3 @@
-
 import { useEffect } from "react";
 import { getAsiakkaatEmail, getTuote, getTuotteet } from "../components/Server/TuoteAPI";
 import React, { useState } from "react";
@@ -24,6 +23,9 @@ const AccountManagement = (props) => {
   const [sahkopostit, setSahkopostit] = useState([]);
   const [tuotteet, setTuotteet] = useState([]);
   const [isLoading, setIsLoading] = useState(true); // set isLoading to true when data is not available
+  const [MuokkaaTilausta, setMuokkaaTilausta] = useState([]);
+  const [viimeisinTilaus, setViimeisinTilaus] = useState([]);
+  const [orderItemQuantity, setOrderItemQuantity] = useState([]);
 
   useEffect(() => {
     const fetchTuotteet = async () => {
@@ -151,6 +153,7 @@ const AccountManagement = (props) => {
     setFormData({ ...formData, [name]: value });
   };
 
+  
   const handleUpdateAsiakas = async (event) => {
     event.preventDefault();
     const newCustomerData = {
@@ -166,6 +169,8 @@ const AccountManagement = (props) => {
         ...newCustomerData
       }
     });
+
+
     editAsiakas(formData, props.userID)
     console.log("Form submitted" + JSON.stringify(formData));
     //clear ostoskori after submit and set tilaus to true
@@ -242,45 +247,60 @@ const AccountManagement = (props) => {
     props.asiakasTiedot.customer.puhelinnro = formData.phone;
     props.asiakasTiedot.customer.osoite = formData.address + ", " + formData.zip + ", " + formData.city;
 
+  const latestOrder = props.asiakasTiedot.orders.reduce((acc, curr) => {
+    return acc.tilausID > curr.tilausID ? acc : curr;
+  });
 
-    const editoredit = formData;
+  const dateToSet = {
+    tilauspvm: "2022-05-04T13:30:00.000Z"
+  };
+  
+  const date = new Date(latestOrder.tilauspvm);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  
+  const formattedDate = `${year}-${month}-${day}`;
+  console.log(formattedDate);
 
-    //const userData = await editOrder(editoredit);
+  const latestOrderItems = props.asiakasTiedot.orderItems.filter(
+    (orderItem) => orderItem.tilausid === latestOrder.tilausID
+  );
 
-    //editAsiakas(JSON.stringify(props.asiakasTiedot.customer));
-    //console.log(JSON.stringify(props.asiakasTiedot.customer));
+  const mytest = {
+    customer: {
+      asiakasID: props.asiakasTiedot.customer.asiakasID,
+      nimi: props.asiakasTiedot.customer.nimi,
+      email: props.asiakasTiedot.customer.email,
+      osoite: props.asiakasTiedot.customer.osoite,
+      puhelinnro: props.asiakasTiedot.customer.puhelinnro,
+    },
+    orders:{
+        tilausID: latestOrder.tilausID,
+        asiakasID: props.asiakasTiedot.customer.asiakasID,
+        tilauspvm: dateToSet,
+        maksuid: latestOrder.maksuid,
+        summa: latestOrder.summa,
+    },
+      orderitem: latestOrderItems.map((orderItem) => {
+        return {
+          tuoteid: orderItem.tuoteid,
+          tilausid: latestOrder.tilausID,
+          kpl: orderItem.kpl /*orderItem.kpl*/,
+          tilaustuotteetid: orderItem.tilaustuotteetid,
+          summa: orderItem.summa,
+        };
+      }),
+  };
 
-    const customer = {
-      asiakasID : props.userID,
-      email: formData.email,
-      nimi: formData.firstName + " " + formData.lastName,
-      osoite: formData.address + ", " + formData.zip + ", " + formData.city,
-      puhelinnro: formData.phone,
-    };
+    console.log("MY DATATEST COMPARE TO MAKE" + JSON.stringify(mytest));
+    console.log("testimake" + JSON.stringify(testimake));
+    console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
 
-    const orders = {
-      tilausID: props.tilausID,
-      asiakasID: props.userID,
-      tilauspvm: props.tilausPvm,
-      maksuid: props.maksuID,
-    };
-    
-    props.setAsiakasTiedot({
-      ...props.asiakasTiedot,
-      customer: {
-        ...props.asiakasTiedot.customer,
-        ...newCustomerData
-      }
-    });
-    
     // If there are no errors, submit the form
     console.log("Kokeillaan PUT")
     console.log(JSON.stringify(testimake))
-    editOrder(testimake);
-    /*
-    const data = {  };
-    await editOrder(data);
-    */
+    editOrder(mytest);
 
     if (Object.keys(errors).length === 0 && props.items.length > 0) {
       props.dataUpdated();
@@ -343,12 +363,6 @@ const AccountManagement = (props) => {
       </div>
     );
   }
-
-
-
-
-
-
 
   return (
     <div
@@ -535,7 +549,7 @@ const AccountManagement = (props) => {
                         size="md"
                         className="btn btn-primary"
                         color="primary"
-                        onClick={handleOrderChange}
+                        onClick={handleSubmit}
                       >
                         Muokkaa tilausta
                         </MDBBtn>
